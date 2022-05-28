7cb76fcb2062dfd13e3059e80aead051fc1fae03
DHEERAJPRAKASH <push.default>
2022-05-28 20:34:43 +0530

    THIRD COMMIT

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -5,10 +5,9 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 var fetchuser = require("../middleware/fetchuser");
-var fetchdoctor = require("../middleware/fetchdoctor");
 const JWT_SECRET = "Dheerajisagoodb$oy";
 
-//ROUTE 1: Create a User using : POST "/api/auth/createuser. Doesn't require Auth. no login required"
+///ROUTE 1: Create a User using : POST "/api/auth/createuser. Doesn't require Auth. no login required"
 router.post(
   "/createuser",
   [
@@ -28,9 +27,9 @@ router.post(
 
     //check whether the user with this email exists already
     try {
-      let user = await User.findOne({ email: req.body.email });
+      let jobuser = await Doctor.findOne({ email: req.body.email });
       // console.log(user);
-      if (user) {
+      if (jobuser) {
         return res
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
@@ -39,7 +38,7 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       const secPass = await bcrypt.hash(req.body.password, salt);
       // Create a new user
-      user = await User.create({
+      jobuser = await Doctor.create({
         name: req.body.name,
         email: req.body.email,
         password: secPass,
@@ -47,7 +46,7 @@ router.post(
 
       const data = {
         user: {
-          id: user.id,
+          id: jobuser.id,
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
@@ -62,7 +61,7 @@ router.post(
 
 //ROUTE 2 - Login user using : POST "/api/auth/login.
 router.post(
-  "/login",
+  "/loginuser",
   [
     body("email", "Enter a valid email").isEmail(),
     body("password", "Password cannot be blank").exists(),
@@ -78,15 +77,15 @@ router.post(
     const { email, password } = req.body;
     try {
       //to check the valid input
-      let user = await User.findOne({ email });
-      if (!user) {
+      let jobuser = await Doctor.findOne({ email });
+      if (!jobuser) {
         return res
           .status(400)
           .json({ error: "Please try to login with correct credentials" });
       }
 
       //compares the given input password with existing password
-      const passwordCompare = await bcrypt.compare(password, user.password);
+      const passwordCompare = await bcrypt.compare(password, jobuser.password);
       if (!passwordCompare) {
         return res
           .status(400)
@@ -95,7 +94,7 @@ router.post(
 
       const data = {
         user: {
-          id: user.id,
+          id: jobuser.id,
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
@@ -108,139 +107,17 @@ router.post(
 );
 
 //ROUTE 3 - Logged in  user details retrieval using : POST "/api/auth/getuser.
-router.post(
-  "/getuser",fetchuser,
-  async (req, res) => {
-
-    try {
-      const userId = req.user.id;
-      const user = await User.findById(userId).select("-password");
-      res.send(user);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
-    }
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    // console.log("success in id")
+    const jobUserId = req.user.id;
+    // console.log(doctorId);
+    const jobUser = await Doctor.findById(jobUserId).select("-password");
+    res.send(jobUserId);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
   }
-);
-
-//ROUTE 4: Create a Doctor using : POST "/api/auth/createdoctor. Doesn't require Auth. no login required"
-router.post(
-  "/createdoctor",
-  [
-    body("name", "Enter a valid name").isLength({ min: 3 }),
-    body("email", "Enter a valid email").isEmail(),
-    body("password", "Password must be atleast 5 characters").isLength({
-      min: 3,
-    }),
-  ],
-  async (req, res) => {
-    //if there are errors, return bad request
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    //check whether the user with this email exists already
-    try {
-      let doctor = await Doctor.findOne({ email: req.body.email });
-      // console.log(user);
-      if (doctor) {
-        return res
-          .status(400)
-          .json({ error: "Sorry a user with this email already exists" });
-      }
-      // Hashing the password
-      const salt = await bcrypt.genSalt(10);
-      const secPass = await bcrypt.hash(req.body.password, salt);
-      // Create a new user
-      doctor = await Doctor.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: secPass,
-      });
-
-      const data = {
-        user: {
-          id: doctor.id,
-        },
-      };
-      const authtoken = jwt.sign(data, JWT_SECRET);
-      // res.json(user);
-      res.json({ authtoken });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
-    }
-  }
-);
-
-//ROUTE 5 - Login doctor using : POST "/api/auth/logindoctor.
-router.post(
-  "/logindoctor",
-  [
-    body("email", "Enter a valid email").isEmail(),
-    body("password", "Password cannot be blank").exists(),
-  ],
-  async (req, res) => {
-    //if there are errors, return bad request
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { email, password } = req.body;
-    try {
-      //to check the valid input
-      let doctor = await Doctor.findOne({ email });
-      if (!doctor) {
-        return res
-          .status(400)
-          .json({ error: "Please try to login with correct credentials" });
-      }
-
-      //compares the given input password with existing password
-      const passwordCompare = await bcrypt.compare(password, doctor.password);
-      if (!passwordCompare) {
-        return res
-          .status(400)
-          .json({ error: "Please try to login with correct credentials" });
-      }
-
-      const data = {
-        user: {
-          id: doctor.id,
-        },
-      };
-      const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({ authtoken });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
-    }
-  }
-);
-
-//ROUTE 3 - Logged in  doctor details retrieval using : POST "/api/auth/getdoctor.
-router.post(
-  "/getdoctor",fetchdoctor,
-  async (req, res) => {
-
-    try {
-      // console.log("success in id")
-      const doctorId = req.doctor.id;
-      // console.log(doctorId);
-      const doctor = await Doctor.findById(doctorId).select("-password");
-      res.send(doctor);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
-    }
-  }
-);
-
-
-
+});
 
 module.exports = router;
--- BACKEND/routes/jobUserRoute.js
+++ BACKEND/routes/jobUserRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-var fetchuser = require("../middleware/fetchdoctor");
+var fetchuser = require("../middleware/fetchuser");
 const DetailJobUser = require("../models/DetailJobUser");
 // const UploadUsers = require("../models/UploadDetail");
 
@@ -43,6 +43,8 @@ router.post(
   ],
   async (req, res) => {
     try {
+      console.log("hai");
+      console.log(req.user.id);
       const { name, mobileno, designation, working } = req.body;
       //if there are errors, return bad request
       const errors = validationResult(req);
@@ -51,9 +53,10 @@ router.post(
         return res.status(400).json({ errors: errors.array() });
       }
       //   console.log(req.user.id);
-      console.log(req.users.id);
+      // console.log(req.users.id);
+
       const userdet = new DetailJobUser({
-        user: req.users.id,
+        user: req.user.id,
         // user:req.user.id,
         name,
         mobileno,
@@ -61,7 +64,7 @@ router.post(
         working,
       });
 
-      //   console.log(userdet);
+      console.log(userdet);
 
       const saveduser = await userdet.save();
 
@@ -103,7 +106,7 @@ router.put("/updateuser/:id", fetchuser, async (req, res) => {
     if (!userupdate) {
       return req.status(404).send("Not Found");
     }
-    if (userupdate.user.toString() !== req.users.id) {
+    if (userupdate.user.toString() !== req.user.id) {
       return req.status(401).send("Not Allowed");
     }
 
@@ -121,7 +124,6 @@ router.put("/updateuser/:id", fetchuser, async (req, res) => {
   }
 });
 
-
 //ROUTE 4 - Logged in  user details deleting details : GET "/api/userdetails/deleteuser.LOGIN REQUIRED
 router.delete("/deleteuser/:id", fetchuser, async (req, res) => {
   try {
@@ -130,206 +132,20 @@ router.delete("/deleteuser/:id", fetchuser, async (req, res) => {
       return req.status(404).send("Not Found");
     }
 
-    if (userupdate.user.toString() !== req.users.id) {
+    if (userupdate.user.toString() !== req.user.id) {
       return req.status(401).send("Not Allowed");
     }
 
     userupdate = await DetailJobUser.findByIdAndDelete(req.params.id);
-    res.json({ Success: "User Details has been deleted", userupdate: userupdate });
+    res.json({
+      Success: "User Details has been deleted",
+      userupdate: userupdate,
+    });
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
   }
 });
 
-
-router.get(
-  "/uploadDetail",
-  fetchuser,
-  // upload.single('userImage'),
-  [
-    // body("dob", "Enter a valid DateofBirth").isDate(),
-    body("name", "Enter a valid Name").isLength({ min: 3 }),
-    body("worknature", "Enter a valid worknature").isLength({ min: 4 }),
-    body("exercisedaily", "Enter a valid value for exercise daily").isBoolean(),
-    body("eatingdiet", "Enter a valid value for eating diet").isBoolean(),
-    body(
-      "alcoholconsumption",
-      "Enter a valid value for alcoholconsumption"
-    ).isBoolean(),
-    body(
-      "caffeineconsumption",
-      "Enter a valid value for caffeineconsumption"
-    ).isBoolean(),
-    body("smoking", "Enter a valid value for smoking").isBoolean(),
-    body("othercomments", "Enter a valid othercomments").isLength({ min: 3 }),
-    body(
-      "list_of_drug_allergies",
-      "Enter a valid list_of_drug_allergies"
-    ).isLength({ min: 3 }),
-    body("other_illnesses", "Enter a valid other_illnesses").isLength({
-      min: 3,
-    }),
-    body("list_of_operations", "Enter a valid list_of_operations").isLength({
-      min: 3,
-    }),
-    body(
-      "list_of_current_medications",
-      "Enter a valid list_of_current_medications"
-    ).isLength({ min: 3 }),
-    // added on 10/01/2022
-    // upload.single('userImage')
-  ],
-  async (req, res) => {
-    try {
-      // added on 10/01/2022
-
-      const {
-        name,
-        worknature,
-        exercisedaily,
-        eatingdiet,
-        alcoholconsumption,
-        caffeineconsumption,
-        smoking,
-        othercomments,
-        list_of_drug_allergies,
-        other_illnesses,
-        list_of_operations,
-        list_of_current_medications,
-        // userImage
-      } = req.body;
-      //if there are errors, return bad request
-      const errors = validationResult(req);
-      // const {data, mimetype}=req.files.userImage;
-      console.log(req.body.smoking);
-
-      // console.log("path of file: "+req.files.userImage.path);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      const userdet = new UploadUsers({
-        user: req.user.id,
-        name,
-        worknature,
-        exercisedaily,
-        eatingdiet,
-        alcoholconsumption,
-        caffeineconsumption,
-        smoking,
-        othercomments,
-        list_of_drug_allergies,
-        other_illnesses,
-        list_of_operations,
-        list_of_current_medications,
-      });
-
-      // upload.single(req.files.userImage);
-      const savedUser = await userdet.save();
-      console.log("saved user" + savedUser);
-
-      res.json([savedUser]);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
-    }
-  }
-);
-
-router.get("/fetchconsult", fetchuser, async (req, res) => {
-  try {
-    const user = req.users.id;
-    console.log(req.users);
-    // console.log("user id:"+user.toString());
-    const doc = user.toString();
-    console.log("user id:" + doc);
-    const detailuser = await UploadUsers.find({ user: req.users.id });
-    // const detailuser = await UploadUsers.find({ user: "61ece8918604f561b005a7cf" });
-    console.log(detailuser);
-    res.json([detailuser]);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-router.put("/fetchconsult/:id", fetchuser, async (req, res) => {
-  const {
-    name,
-    worknature,
-    exercisedaily,
-    eatingdiet,
-    alcoholconsumption,
-    caffeineconsumption,
-    smoking,
-    othercomments,
-    list_of_drug_allergies,
-    other_illnesses,
-    list_of_operations,
-    list_of_current_medications,
-    userComments,
-  } = req.body;
-
-  try {
-    //Create a newUser object
-    const newUser = {};
-    console.log("I am id:" + req.params.id);
-    //validate and check which field is available and update accordingly
-    if (name) {
-      newUser.name = name;
-    }
-    if (worknature) {
-      newUser.worknature = worknature;
-    }
-
-    newUser.exercisedaily = exercisedaily;
-    newUser.eatingdiet = eatingdiet;
-    newUser.alcoholconsumption = alcoholconsumption;
-    newUser.caffeineconsumption = caffeineconsumption;
-    newUser.smoking = smoking;
-
-    if (othercomments) {
-      newUser.othercomments = othercomments;
-    }
-    if (list_of_drug_allergies) {
-      newUser.list_of_drug_allergies = list_of_drug_allergies;
-    }
-    if (other_illnesses) {
-      newUser.other_illnesses = other_illnesses;
-    }
-    if (list_of_operations) {
-      newUser.list_of_operations = list_of_operations;
-    }
-    if (list_of_current_medications) {
-      newUser.list_of_current_medications = list_of_current_medications;
-    }
-    if (userComments) {
-      newUser.userComments = userComments;
-    }
-
-    //Find the userdetail to be updated and update it
-    //Always validate the user and update which is done below
-    let userupdate = await UploadUsers.findById(req.params.id);
-    if (!userupdate) {
-      return req.status(404).send("Not Found");
-    }
-
-    console.log("user update:" + req.body);
-    console.log("user id:" + req.user.id);
-    if (userupdate.user.toString() !== req.users.id) {
-      return req.status(401).send("Not Allowed");
-    }
-
-    userupdate = await UploadUsers.findByIdAndUpdate(
-      req.params.id,
-      { $set: newUser },
-      { new: true }
-    );
-    res.json({ userupdate });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
-});
 
 module.exports = router;
